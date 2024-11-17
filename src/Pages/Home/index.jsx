94856d65e0c3d5005e48c24db5e741dfcf7d376b
maria <maria.eduarda.schwinn@gmail.com>
2024-11-17 12:25:07 -0300

    Add ContactSection to Home component for improved user interaction

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,34 +1,35 @@
-import { LimitedWidthWrapper } from "../../components/LimitedWidth"
-import { ProjectsSection } from "./sections/Projects"
-import { ServicesSection } from "./sections/Services"
-import { Container, Main, Footer } from "./styles"
-import { CareerSection } from "./sections/Career"
-import { HeaderSection } from "./sections/Header"
-import { AboutSection } from "./sections/About"
-import { InfoSection } from "./sections/Info"
+import { LimitedWidthWrapper } from "../../components/LimitedWidth";
+import { ProjectsSection } from "./sections/Projects";
+import { ServicesSection } from "./sections/Services";
+import { ContactSection } from "./sections/Contact";
+import { Container, Main, Footer } from "./styles";
+import { CareerSection } from "./sections/Career";
+import { HeaderSection } from "./sections/Header";
+import { AboutSection } from "./sections/About";
+import { InfoSection } from "./sections/Info";
 
 export function Home() {
-  return (
-    <Container>
-      <HeaderSection />
-      <LimitedWidthWrapper>
-        <Main>
-          <InfoSection />
-          <AboutSection />
-          <ServicesSection />
-          <CareerSection />
-          <ProjectsSection />
-          {/* <ContactSection /> */}
-          {/* SOFT/HARD SKILLS? ideias em: https://www.youtube.com/watch?v=d8MReiqqElE min=9:20*/}
-          {/* certificados do LinkedIn?*/}
-        </Main>
-        <Footer>
-          <p>
-            &copy; {new Date().getFullYear()} Portfólio de Maria Eduarda
-            Rodrigues Schwinn
-          </p>
-        </Footer>
-      </LimitedWidthWrapper>
-    </Container>
-  )
+    return (
+        <Container>
+            <HeaderSection />
+            <LimitedWidthWrapper>
+                <Main>
+                    <InfoSection />
+                    <AboutSection />
+                    <ServicesSection />
+                    <CareerSection />
+                    <ProjectsSection />
+                    <ContactSection />
+                    {/* SOFT/HARD SKILLS? ideias em: https://www.youtube.com/watch?v=d8MReiqqElE min=9:20*/}
+                    {/* certificados do LinkedIn?*/}
+                </Main>
+                <Footer>
+                    <p>
+                        &copy; {new Date().getFullYear()} Portfólio de Maria
+                        Eduarda Rodrigues Schwinn
+                    </p>
+                </Footer>
+            </LimitedWidthWrapper>
+        </Container>
+    );
 }
