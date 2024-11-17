a20245b1d0de82810039fb2d6ada68cd392ebb51
maria <maria.eduarda.schwinn@gmail.com>
2024-11-17 12:25:37 -0300

    Comment out feedback form and axios functionality in ContactSection

--- src/Pages/Home/sections/Contact/index.jsx
+++ src/Pages/Home/sections/Contact/index.jsx
@@ -1,61 +1,59 @@
-import localization_icon from "../../../../assets/icons/localization.svg"
-import telephone_icon from "../../../../assets/icons/telephone.svg"
-import { ContactBox } from "../../../../components/ContactBox"
-import email_icon from "../../../../assets/icons/email.svg"
-import { Subtitle } from "../../../../components/Subtitle"
-import { Content, FormArea } from "./styles"
-import { useState } from "react"
-import axios from "axios"
+import localization_icon from "../../../../assets/icons/localization.svg";
+import telephone_icon from "../../../../assets/icons/telephone.svg";
+import { ContactBox } from "../../../../components/ContactBox";
+import email_icon from "../../../../assets/icons/email.svg";
+import { Subtitle } from "../../../../components/Subtitle";
+import { Content } from "./styles";
 
 export function ContactSection() {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [message, setMessage] = useState("")
+    // const [name, setName] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [message, setMessage] = useState("");
 
-  const handleSubmit = async (event) => {
-    event.preventDefault()
-    try {
-      const response = await axios.post("/.netlify/functions/feedback", {
-        name,
-        email,
-        message,
-      })
-      console.log(response.data.message)
-      alert("Feedback enviado com sucesso!")
-      setName("")
-      setEmail("")
-      setMessage("")
-    } catch (error) {
-      console.error("Erro ao enviar feedback", error)
-      console.error("Erro ao enviar feedback!")
-    }
-  }
+    // const handleSubmit = async (event) => {
+    //     event.preventDefault();
+    //     try {
+    //         const response = await axios.post("/.netlify/functions/feedback", {
+    //             name,
+    //             email,
+    //             message,
+    //         });
+    //         console.log(response.data.message);
+    //         alert("Feedback enviado com sucesso!");
+    //         setName("");
+    //         setEmail("");
+    //         setMessage("");
+    //     } catch (error) {
+    //         console.error("Erro ao enviar feedback", error);
+    //         console.error("Erro ao enviar feedback!");
+    //     }
+    // };
 
-  return (
-    <div id="contact">
-      <Subtitle content="Contato" />
-      <Content>
-        <div>
-          <h2>Contatar</h2>
-          <ContactBox
-            link="mailto:maria.eduarda.schwinn@gmail.com"
-            icon={email_icon}
-            title="Email"
-            description="maria.eduarda.schwinn@gmail.com"
-          />
-          <ContactBox
-            link="tel:+5551980622891"
-            icon={telephone_icon}
-            title="Telefone"
-            description="+55 (51) 98062-2891"
-          />
-          <ContactBox
-            icon={localization_icon}
-            title="Endereço"
-            description="São Martinho, Santa Cruz do Sul - RS, Brasil"
-          />
-        </div>
-        <FormArea>
+    return (
+        <div id="contact">
+            <Subtitle content="Contato" />
+            <Content>
+                <div>
+                    <h2>Contatar</h2>
+                    <ContactBox
+                        link="mailto:maria.eduarda.schwinn@gmail.com"
+                        icon={email_icon}
+                        title="Email"
+                        description="maria.eduarda.schwinn@gmail.com"
+                    />
+                    <ContactBox
+                        link="tel:+5551980622891"
+                        icon={telephone_icon}
+                        title="Telefone"
+                        description="+55 (51) 98062-2891"
+                    />
+                    <ContactBox
+                        icon={localization_icon}
+                        title="Endereço"
+                        description="São Martinho, Santa Cruz do Sul - RS, Brasil"
+                    />
+                </div>
+                {/* <FormArea>
           <h2>Comentário</h2>
           <form onSubmit={handleSubmit}>
             <label htmlFor="name">Digite seu nome</label>
@@ -85,8 +83,8 @@ export function ContactSection() {
             />
             <button type="submit">Enviar mensagem</button>
           </form>
-        </FormArea>
-      </Content>
-    </div>
-  )
+        </FormArea> */}
+            </Content>
+        </div>
+    );
 }
